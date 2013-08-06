352eeeafc41e39d84936accb04f5fcd2d9c4c6ed
dbc-apprentice <jamescarney10@gmail.com>
2013-08-06 10:20:59 -0500

    rspec tests up and running

--- cookie_spec.rb
+++ cookie_spec.rb
@@ -22,13 +22,21 @@ describe Cookie do
   end
 
   describe "#type" do
-    it "returns the type of the cookie"
+    context "with invalid input" do
+      it "returns the type of the cookie" do
+          cookie.type.should eq(type)
+      end
+    end
   end
 
   describe "#bake!" do
-    it "requires an integer time argument"
+    it "requires an integer time argument" do
+      expect { cookie.bake!("10")}.to raise_error(TypeError)
+    end
 
-    it "returns the cookie object"
+    it "returns the cookie object" do
+      cookie.bake!(10).should be_an_instance_of Cookie
+    end
 
     it "changes the status of the cookie when given enough time" do
       expect { cookie.bake!(10) }.to change(cookie, :status)
@@ -36,26 +44,39 @@ describe Cookie do
   end
 
   describe "#status" do
-    it "returns the cookie's current status"
+    it "returns the cookie's current status" do
+      expect { cookie.bake!(7) }.to change(cookie, :status)
+    end
 
     context "when unbaked" do
-      it "is `:doughy`" 
+      it "is `:doughy`" do
+        cookie.bake!(6).status.should eq(:doughy)
+        #cookie.status.should eq(:doughy)
+      end
     end
 
     context "when baked for less than 7 minutes" do
-      it "is `:doughy`"
-    end  
+      it "is `:doughy`" do
+        cookie.bake!( rand(7) ).status.should eq(:doughy)
+      end
+    end
 
     context "when baked for at least 7 but less than 10 minutes" do
-      it "is `:almost_ready`"
+      it "is `:almost_ready`" do
+        cookie.bake!( rand(7..9) ).status.should eq(:almost_ready)
+      end
     end
 
     context "when baked for at least 10 but less than 12 minutes" do
-      it "is `:ready`"
+      it "is `:ready`" do
+        cookie.bake!( rand(10..12) ).status.should eq(:ready)
+      end
     end
 
     context "when baked for at least 12 minutes" do
-      it "is `:burned`"
+      it "is `:burned`" do
+        cookie.bake!( 12 ).status.should eq(:burned)
+      end
     end
   end
 end
